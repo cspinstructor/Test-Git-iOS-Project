d05a167320add56323f0ebcdee1f278a1384eb0f
Paul Chin <paulchin@Pauls-MacBook-Air.local>
2014-12-21 00:21:39 +0800

    added a test class

---
+++ Git Test/TestNewFile.h
@@ -0,0 +1,13 @@
+//
+//  TestNewFile.h
+//  Git Test
+//
+//  Created by Paul Chin on 12/21/14.
+//  Copyright (c) 2014 Paul Chin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestNewFile : NSObject
+
+@end
---
+++ Git Test/TestNewFile.m
@@ -0,0 +1,13 @@
+//
+//  TestNewFile.m
+//  Git Test
+//
+//  Created by Paul Chin on 12/21/14.
+//  Copyright (c) 2014 Paul Chin. All rights reserved.
+//
+
+#import "TestNewFile.h"
+
+@implementation TestNewFile
+
+@end
